9ce40de5c22d2d9fdb26a85dda90ea3fb461e737
fredfaya <96979843+fredfaya@users.noreply.github.com>
2022-06-07 00:01:10 +0100

    Add files via upload
    Ajout de la partie frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AcceuilComponent } from './Components/acceuil/acceuil.component';
+import { ResultComponent } from './Components/result/result.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NoResultComponent } from './Components/no-result/no-result.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcceuilComponent,
+    ResultComponent,
+    NoResultComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
